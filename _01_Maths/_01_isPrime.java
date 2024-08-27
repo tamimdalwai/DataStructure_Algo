2ebcedde215d496153ea4c3bc0fbeb54dcf5dac7
Tamim Dalwai <tamimdalwai@gmail.com>
2024-08-27 22:40:55 +0530

    basic maths

---
+++ _01_Maths/_01_isPrime.java
@@ -0,0 +1,25 @@
+package _01_Maths;
+
+/**
+ * _01_isPrime
+ */
+public class _01_isPrime {
+
+    public static boolean isPrime(int N){
+        for(int i=2;i<=N-1;i++){
+            if(N % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public 
+
+    public static void main(String[] args) {
+        // System.out.println("the number is prime : "+isPrime(7));
+
+
+        System.out.println("the number is prime : "+isPrime(8));
+    }
+}
---
+++ _01_Maths/_02_printPrime.java
@@ -0,0 +1,5 @@
+package _01_Maths;
+
+public class _02_printPrime {
+    
+}
